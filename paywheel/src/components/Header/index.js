40bd574d09520be2f00454546591c3aa76f7c63e
Ravindra Nath <ravindranath@Ravindras-MacBook-Air.local>
2020-10-18 15:37:38 +0530

    fix

--- paywheel/src/components/Header/index.js
+++ paywheel/src/components/Header/index.js
@@ -35,7 +35,7 @@ const Header = (props) => {
             
         </div>
           <div style={{margin: '20px 0', color: '#fff', fontWeight: 'bold'}}>
-            {auth.authenticated ? `Hi ${auth.firstName} ${auth.lastName}` : ''}
+            {auth.authenticated ? ` ${auth.firstName}'s Calender ` : ''}
           </div>
         <ul className="menu">
 
